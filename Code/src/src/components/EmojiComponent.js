ce3a1663e25071fa3cba9c6ead71a55eb9a0ae31
Nick Lesho <nick.lesho@gmail.com>
2018-10-28 16:33:07 -0400

    Refactored emoji and number file so that a loop creates the object on the screen instead

--- Code/src/src/components/EmojiComponent.js
+++ Code/src/src/components/EmojiComponent.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react';
 
 class Emojis extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { type: props.statement };
+    this.createButtons = this.createButtons.bind(this);
+  }
+
+  createButtons() {
+    let buttons = [];
+    let emojiList = ['😄','😌','😀','😍','😎','😡','😅','😴','🙄','😲','😋',
+      '😭','😈','😇','🤑','🤵','👰','🤳','🙈','🙉','🙊','🎅','❤️','💔'];
+    for (let i = 0; i < 24; i++) {
+      var rowNum = i / 6 + 1;
+      var colNum = i % 6 + 1;
+      var emoji = emojiList[i];
+      var className = "entry row" + rowNum + " col" + colNum;
+      buttons.push(<button className={className}>{emoji}</button>);
+      if (i !== 0 && (i+1) % 6 === 0) {
+        buttons.push(<br />)
+      }
+    }
+    return buttons;
+  }
+
+  render() {
+    return (
+      <div className="userInput">
+        {this.createButtons()}
+      </div>
+    )
+  }
+}
+/*
   render(){
     return (
       <div className="userInput">
@@ -34,6 +67,7 @@ class Emojis extends Component {
       </div>
     )
   }
-}
+  */
+
 
 export default Emojis;
--- Code/src/src/components/NumberComponent.js
+++ Code/src/src/components/NumberComponent.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 
 class Numbers extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { type: props.statement };
+    this.createButtons = this.createButtons.bind(this);
+  }
+
+  createButtons() {
+    let buttons = [];
+    for (let i = 0; i < 10; i++) {
+      var rowNum = i / 5 + 1;
+      var colNum = i % 5 + 1;
+      var number = i;
+      var className = "entry-wide row" + rowNum + " col" + colNum;
+      buttons.push(<button className={className}>{number}</button>);
+      if (i !== 0 && (i+1) % 5 === 0) {
+        buttons.push(<br />)
+      }
+    }
+    return buttons;
+  }
+
+/*
   render() {
     return (
       <div className="userInput">
@@ -16,6 +39,15 @@ class Numbers extends Component {
         <button className="entry-wide">8</button>
         <button className="entry-wide">9</button>
 
+      </div>
+    )
+  }
+  */
+
+  render() {
+    return (
+      <div className="userInput">
+        {this.createButtons()}
       </div>
     )
   }
